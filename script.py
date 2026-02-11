6b1adb53e05cb7b6bc216043678f3dc1df53c4cc
platonzhuman <platonzhuman@bk.ru>
2026-02-11 16:09:47 +0800

    udpate script.py

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 def add(a,b):
 	return a * b
 def division(a, b):
+	if b == 0:
+		raise ValueError("sorry error")
 	return a / b
 
 print(add(30, 12))
-print(division(30, 10))
+print(division(10, 0))
 
 
