29ee6f0be1981fe900c342a5144c9cff27b310dc
platonzhuman <platonzhuman@bk.ru>
2026-02-25 16:07:48 +0800

    download pinger.py

---
+++ pinger.py
@@ -0,0 +1,7 @@
+import requests
+
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
+date = "2025-02-19"
+url = url_template.format(date=date)
+response = requests.get(url=url, stream=True)
+print(f"for {date} got: {response}")
