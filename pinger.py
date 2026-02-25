09803b030e84a5ffca1162e8f402090aa136c74b
platonzhuman <platonzhuman@bk.ru>
2026-02-25 16:29:40 +0800

    reform pinger.py again

--- pinger.py
+++ pinger.py
@@ -1,10 +1,16 @@
 import requests
-from datetime import datetime
+from datetime import datetime,timedelta
 
+url_teamplate="https://simurg.space/gen_file?data=obs&date={date}"
+current=datetime.now()
+while True:
+    date=current.strftime("%Y-%m-%d")
+    url=url_teamplate.format(date=date)
 
-url_template = "https://simurg.space/gen_file?data=obs&date={date}"
-now = datetime.now()
-date = now.strftime("%Y-%m-%d")
-url = url_template.format(date=date)
-response = requests.get(url=url, stream=True)
-print(f"for {date} got: {response}")
+    response=requests.get(url=url,stream=True)
+    print(f"for {date} got: {response}")
+    if response.status_code ==200:
+        print(f"last available data are for {date}")
+        break
+    else:
+        current=current-timedelta(days=1)
