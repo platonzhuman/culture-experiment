4f9f19fbb9872c3b9aa629bb64695f174a1a36e5
platonzhuman <platonzhuman@bk.ru>
2026-03-04 16:29:19 +0800

    show redundant test principle

--- testing/tests/tests_principles.py
+++ testing/tests/tests_principles.py
@@ -22,10 +22,20 @@ def test_addition_duplicated():
     #it is real good test 
     assert add(2, 3) == 2 + 3
 
+def test_addition_overcomplicated():
+    # formally valid test but too slow
+    for i in range(0, 2**32):
+        for j in range(0, 2**32):
+            assert add(i, j) == sum([i, j]) #might dublicate principles
+            assert add(-i, j) == sum([-i, j])
+            assert add(i, -j) == sum([i, -j])
+            assert add(-i, -j) == sum([-i, -j])
+
+
 
 
 if __name__=="__main__":
     test_addition()
     test_addition_with_bug()
     test_addition_duplicated()
-    
+    # test_addition_overcomplicated()
