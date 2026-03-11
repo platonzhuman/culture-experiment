3b25c4e548fcdc30c902bd0b90463f30b9940ddb
platonzhuman <platonzhuman@bk.ru>
2026-03-11 16:56:00 +0800

    update and start work file

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -9,14 +9,17 @@ from ndfl import calculate_ndfl_tax
 # | **Свыше 50 млн руб.** | 22% | 9 402 000 + 22% с суммы превышения |
 
 
-def test_ndfl():
-    assert test_ndfl() == None
+# def test_ndfl():
+#     assert test_ndfl() == None
+
+# def calculate_ndfl_tax():
+#     assert calculate_ndfl_tax() == None
 
 def test_ndfl_tier_1():
     assert calculate_ndfl_tax(500_000) == 65_000
 
 def test_ndfl_tier_2():
-    assert calculate_ndfl_tax(4_000_000) == 512_000
+    assert calculate_ndfl_tax(4_000_000) == 552_000
 
 def test_ndfl_tier_3():
     assert calculate_ndfl_tax(10_000_000) == 1_602_000
