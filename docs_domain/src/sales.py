b97ab32a7bfc7f185e9c228a82d69393b4c16c5d
platonzhuman <platonzhuman@bk.ru>
2026-04-01 16:49:06 +0800

    update and reform code in docs_domain

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -1,25 +1,25 @@
 # TODO refactor this module using buisness logic names
 
 
-def _row(x):
-    # x is one line from file
-    p = x.strip().split(",")  # split by comma
-    if len(p) != 4:  # if line is bad
-        return None  # return nothing
 
-    n = p[0]  # product name
-    c = p[1]  # product category
-    a = float(p[2])  # price of one item
-    q = int(p[3])  # amount of items
+def _parse_record(line:str):
+    sale = line.strip().split(",") 
+    if len(sale) != 4:  # according specs all sales have 4 cols
+        return None  
 
-    return {"n": n, "c": c, "a": a, "q": q}  # make dict
+    product_name = sale[0]  
+    category = sale[1]  
+    unit_price = float(sale[2]) 
+    quanity = int(sale[3]) 
+
+    return {"product_name": product_name, "category": category, "unit_price": unit_price, "quanity": quanity}  
 
 
 def read_data(path):
     res = []  # final list
     with open(path, "r", encoding="utf-8") as f:  # open file
         for x in f:  # go over lines
-            r = _row(x)  # convert line to dict
+            r = _parse_record(x)  # convert line to dict
             if r is not None:  # if parsing was ok
                 res.append(r)  # add to result
     return res  # return result
