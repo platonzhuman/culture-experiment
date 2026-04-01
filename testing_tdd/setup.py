243bac3628589ec0732a61e21ce0219e96a3643e
platonzhuman <platonzhuman@bk.ru>
2026-04-01 16:11:22 +0800

    Save all changes including setup.py

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -5,8 +5,10 @@ from setuptools import (
 )
 
 setup(
-    name="ndfl",
+    name="Platonxa_ndfl",
     version= "0.0.0",
+    long_description='calculate_ndfl_lax',
+    long_description_content_type= "text/markdown",
     package_dir= {"":"src"},
     packages = find_packages(where="src")
 )
