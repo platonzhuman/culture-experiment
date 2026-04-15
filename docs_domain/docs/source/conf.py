24887dbd12894b8c1f57c47c18047235e8a70132
platonzhuman <platonzhuman@bk.ru>
2026-04-15 16:21:13 +0800

    Added markdown support

--- docs_domain/docs/source/conf.py
+++ docs_domain/docs/source/conf.py
@@ -14,7 +14,14 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+]
+
+sourse_suffic = {
+    ".md": "markdown",
+    ".rst": "restructuredtext", 
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
